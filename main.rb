681fca9ffa60b7dce74c671224bf2abe11b9ccf0
Justin B <justin.d.barry@gmail.com>
2010-11-09 14:57:20 -0800

    moving back to associative array

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ require 'lib/dns_resolve'
 post '/resolve_a_record_to_ip' do
   dnsresolve = DNSResolve.new
   result = JSON.parse(request.body.read) 
-  puts result
+  
   result = dnsresolve.resolve_a_record_to_IP(result['host_name'])
   
   content_type :json
--- test/test_main.rb
+++ test/test_main.rb
@@ -11,12 +11,12 @@ ENV['RACK_ENV'] = 'test'
 class Test_Main < Test::Unit::TestCase
   include Rack::Test::Methods
   def app
-     Main
+    Sinatra::Application
   end
 
   def test_resolve_a_record_to_ip
-    post '/resolve_a_record_to_ip', {:host_name => 'honey.com'}.to_json
+    post '/resolve_a_record_to_ip', {:honey_com => 'honey.com', :emergeinteractive_com => 'emergeinteractive.com'}.to_json
     result = JSON.parse last_response.body
-    assert result['ip_address']
+    assert result.has_key? 'honey_com' && result.has_key? 'emergeinteractive_com'
   end
 end
