4fffa928a85954e24a1522fef447715b4cc0eb8d
Justin B <justin.d.barry@gmail.com>
2010-11-09 14:54:30 -0800

    revert last commit

--- main.rb
+++ main.rb
@@ -5,11 +5,10 @@ require 'lib/dns_resolve'
 
 post '/resolve_a_record_to_ip' do
   dnsresolve = DNSResolve.new
-  result = {}
-  JSON.parse(request.body.read).each do |record|
-    puts "#{record.key}-#{record.value}"
-    result.store(record.key, dnsresolve.resolve_a_record_to_IP(host_name))
-  end
+  result = JSON.parse(request.body.read) 
+  puts result
+  result = dnsresolve.resolve_a_record_to_IP(result['host_name'])
+  
   content_type :json
-  result.to_json
+  {:ip_address => result}.to_json
 end
