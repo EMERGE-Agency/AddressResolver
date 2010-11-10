ae590feb216ad0534da327112c7eeadf8b824588
Justin B <justin.d.barry@gmail.com>
2010-11-10 13:07:27 -0800

    fixes

--- test/test_main.rb
+++ test/test_main.rb
@@ -24,27 +24,9 @@ class Test_Main < Test::Unit::TestCase
   end
   
   def test_page_not_found_code_check
-    post '/get_page_response_code', ['honey.com', 'emergeinteractive.com'].to_json
+    post '/get_page_response_code', ['honey.com', 'emergeinteractive.com', 'www.irispmt.com'].to_json
     result = JSON.parse last_response.body
-    assert result.has_key?('honey.com') && result.has_key?('emergeinteractive.com')
-    assert  result['honey.com'] != '200' && result['emergeinteractive.com'] != '200'
-  end
-  
-  def test_http_syntax
-    input = ['honey.com', 'emergeinteractive.com'].to_json
-    result = {}
-    JSON.parse(input).each do |value|
-      res = ""
-      uri = URI.parse("http://www.#{value}")
-      http_session = Net::HTTP.new(uri.host, uri.port)
-      http_session.use_ssl = true if uri.port == 443
-      http_session.start{|http|
-        res = http.get('/?e=actionNotFoundBecauseItDontExist')
-      }
-      result[value] = res.code
-    end
-    assert result.has_key?('honey.com') && result.has_key?('emergeinteractive.com')
-    assert result['honey.com'] === '404' && result['emergeinteractive.com'] === '404'
-    
+    assert result.has_key?('honey.com') && result.has_key?('emergeinteractive.com') && result.has_key?('www.irispmt.com')
+    assert  result['honey.com'] != '200' && result['emergeinteractive.com'] != '200' && result['www.irispmt.com'] != '200'
   end
 end
