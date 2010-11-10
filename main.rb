27906dfa1229cac6f863452f7a555efb5cdd9a49
Justin B <justin.d.barry@gmail.com>
2010-11-09 17:11:38 -0800

    added try catch to timeout method

--- main.rb
+++ main.rb
@@ -11,7 +11,14 @@ post '/resolve_a_record_to_ip' do
   JSON.parse(request.body.read).each_pair do |key, value|
     threads << Thread.new{
       puts "#{key}-#{value}";
-      Timeout::timeout(2){result.store(key, dnsresolve.resolve_a_record_to_IP(value))}}.join
+      ip = ""
+      begin
+        Timeout::timeout(2){ ip = dnsresolve.resolve_a_record_to_IP(value)}        
+      rescue Timeout::Error
+        ip = ""
+      end
+      result.store(key, ip)
+    }.join
   end
   
   content_type :json
