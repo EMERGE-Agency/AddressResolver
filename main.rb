1a02b10228b883ee2fd7396bb92665e754056706
Justin B <justin.d.barry@gmail.com>
2010-11-09 17:29:50 -0800

    moved to just url, no client key

--- main.rb
+++ main.rb
@@ -8,16 +8,16 @@ post '/resolve_a_record_to_ip' do
   dnsresolve = DNSResolve.new
   result = {}
   threads = []
-  JSON.parse(request.body.read).each_pair do |key, value|
+  JSON.parse(request.body.read).each do |value|
     threads << Thread.new{
-      puts "#{key}-#{value}";
+      puts "#{value}";
       ip = ""
       begin
         Timeout::timeout(2){ ip = dnsresolve.resolve_a_record_to_IP(value)}        
       rescue Timeout::Error
         ip = ""
       end
-      result.store(key, ip)
+      result.store(value, ip)
     }.join
   end
   
